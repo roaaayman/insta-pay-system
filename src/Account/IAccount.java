50437a25280337faf51968d79ada31119d19775f
roaaayman <119889441+roaaayman@users.noreply.github.com>
2023-11-11 17:17:54 +0200

    user accounts

--- src/Account/IAccount.java
+++ src/Account/IAccount.java
@@ -1,13 +1,9 @@
 package Account;
 
 public interface IAccount {
-<<<<<<<<< Temporary merge branch 1
     double getBalance();
     void setBalance(double b);
     void createAccount();
     void transfer(IAccount destAcc, double amount);
     void deductAmount(double amount);
-=========
-
->>>>>>>>> Temporary merge branch 2
 }
---
+++ src/User/BankUser.java
@@ -0,0 +1,70 @@
+package User;
+
+import Account.IAccount;
+
+public class BankUser implements User {
+    private String username;
+    private String password;
+    private String mobileNumber;
+    private IAccount account;
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    @Override
+    public IAccount getAccount() {
+        return account;
+    }
+
+    @Override
+    public void signUp(String username, String password, String mobileNumber, IAccount account) {
+        this.username = username;
+        this.password = password;
+        this.mobileNumber = mobileNumber;
+        this.account = account;
+        System.out.println("Bank user registered successfully.");
+    }
+
+    @Override
+    public void signIn(String username, String password) {
+        if (this.username.equals(username) && this.password.equals(password)) {
+            System.out.println("Bank user authenticated successfully.");
+        } else {
+            System.out.println("Authentication failed. Invalid username or password.");
+        }
+    }
+
+    @Override
+    public double inquireBalance() {
+        return account.getBalance();
+    }
+
+    @Override
+    public void payBills() {
+        // Implement utility bill payment for BankUser
+        System.out.println("BankUser paying bills.");
+    }
+
+    @Override
+    public UserProfile viewProfile() {
+        return new UserProfile(username, mobileNumber);
+    }
+
+    @Override
+    public void displayAdditionalDetails() {
+        System.out.println("Account Type: Bank");
+        // Include any additional details specific to BankUser here
+    }
+}
