8bbe124f76a67a077b23ca546088603674ddf245
YoussefKamal1204 <youssef.kamal1204@gmail.com>
2023-11-13 17:02:24 +0200

    bank api done

---
+++ src/Dummy/DummyBankFactory.java
@@ -0,0 +1,22 @@
+package Dummy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DummyBankFactory {
+
+    public static List<Bank> createBanks() {
+        List<Bank> banks = new ArrayList<>();
+
+        // Bank 1
+        banks.add(new Bank("BankA", "123456789"));
+
+        // Bank 2
+        banks.add(new Bank("BankB", "987654321"));
+
+        // Bank 3
+        banks.add(new Bank("BankC", "555566667"));
+
+        return banks;
+}
+}
