cd2f4bb148c54c91718f3588fd386d30894bb7fc
YoussefKamal1204 <youssef.kamal1204@gmail.com>
2023-11-14 21:19:10 +0200

    Bill demo done succefully

--- src/BillData/BankUserBills.java
+++ src/BillData/BankUserBills.java
@@ -16,11 +16,11 @@ public class BankUserBills {
         IBIllPaymentStrategy water = new WaterBillPaymentStrategy();
         IBIllPaymentStrategy electricity = new ElectricityBillPaymentStrategy();
 
-        bills.add(new GasBill("123456789", 100.0, gas));
+        bills.add(new GasBill("1234567899", 100.0, gas));
         bills.add(new GasBill("987654321", 75.0, gas));
         bills.add(new WaterBill("123456789", 50.0, water));
         bills.add(new ElectricityBill("555566667", 120.0, electricity));
 
         return bills;
-    }
+}
 }
